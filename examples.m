8c5cce2681ee7014c365c915e3540b89163ef5b2
Yuanzhao Zhang <zhangyuanzhao45@gmail.com>
2019-06-22 11:44:09 -0500

    updated examples

--- examples.m
+++ examples.m
@@ -1,6 +1,6 @@
 %% Examples accompying the simultaneous block diagonalization code sbd_fast.m
-%% Including applications to cluster synchronization, coupled nonidentical oscillators,
-%% and networks with nonidentical interactions.
+%% Including applications to synchronization patterns arising from symmetry clusters,
+%% Laplacian clusters, equitable clusters, as well as chimera states in multilayer networks
 
 clc;clear;
 
@@ -21,7 +21,7 @@ adj(5,11) = 0; adj(11,5) = 0;
 adj(8,10) = 0; adj(10,8) = 0;
 adj(10,11) = 0; adj(11,10) = 0;
 %% add noise/error to the matrix data
-D{1} = adj + 1e-10*rand(n);
+D{1} = adj + 1e-5*rand(n);
 
 %% D{2} to D{6} are diagonal matrices that encode cluster info
 %% e.g., node 1 and node 8 are in the same symmetry cluster
@@ -47,13 +47,13 @@ D{6} = zeros(n);
 D{6}(11,11) = 1;
 
 %% calculate orthogonal matrix for simultaneous block-diagonalization
-P = sbd_fast(D,'real',1e-5);
+P = sbd_fast(D,'real',1e-2);
 for ii = 1:6
 	D{ii} = P' * D{ii} * P;
 end
 
-set(0,'DefaultAxesFontSize',25)
 
+%% plot the matrices in common block-diagonal form
 figure
 colormap(flipud(colormap));
 set(gcf, 'PaperPosition', [0 0 8 5]);
@@ -88,7 +88,7 @@ adj(2,4) = 0; adj(4,2) = 0;
 rowsum = sum(adj,2);
 laplacian = diag(rowsum) - adj;
 %% add noise/error to the matrix data
-D{1} = laplacian + 1e-8*rand(n);
+D{1} = laplacian + 1e-5*rand(n);
 
 %% D{2} and D{3} are diagonal matrices that encode cluster info
 %% e.g., node 1, 3 and 5 are in the same Laplacian cluster
@@ -102,13 +102,12 @@ D{3}(2,2) = 1;
 D{3}(4,4) = 1;
 
 %% calculate orthogonal matrix for simultaneous block-diagonalization
-P = sbd_fast(D,'real',1e-5);
+P = sbd_fast(D,'real',1e-2);
 for ii = 1:3
 	D{ii} = P' * D{ii} * P;
 end
 
-set(0,'DefaultAxesFontSize',25)
-
+%% plot the matrices in common block-diagonal form
 figure
 colormap(flipud(colormap));
 set(gcf, 'PaperPosition', [0 0 20 5]);
@@ -128,7 +127,7 @@ saveas(gcf,'sorrentino16.pdf')
 clc;clear;
 
 %%%%%%%%%%%%%%%%%%
-% Network with 2 external equitable clusters from
+% Network with 2 equitable clusters from
 % M. T. Schaub, N. O’Clery, Y. N. Billeh, J.-C. Delvenne, R. Lambiotte, and M. Barahona, 
 % Graph partitions and cluster synchronization in networks of oscillators, 
 % Chaos, 26 (2016), 094821.
@@ -148,10 +147,10 @@ adj = [0,1,1,0,0,0,0,0;...
 rowsum = sum(adj,2);
 laplacian = diag(rowsum) - adj;
 %% add noise/error to the matrix data
-D{1} = laplacian + 1e-6*rand(n);
+D{1} = laplacian + 1e-5*rand(n);
 
 %% D{2} and D{3} are diagonal matrices that encode cluster info
-%% e.g., node 3 and node 6 are in the same external equitable cluster
+%% e.g., node 3 and node 6 are in the same equitable cluster
 D{2} = zeros(n);
 D{2}(3,3) = 1;
 D{2}(6,6) = 1;
@@ -159,13 +158,12 @@ D{2}(6,6) = 1;
 D{3} = eye(n) - D{2};
 
 %% calculate orthogonal matrix for simultaneous block-diagonalization
-P = sbd_fast(D,'real',1e-4);
+P = sbd_fast(D,'real',1e-2);
 for ii = 1:3
 	D{ii} = P' * D{ii} * P;
 end
 
-set(0,'DefaultAxesFontSize',25)
-
+%% plot the matrices in common block-diagonal form
 figure
 colormap(flipud(colormap));
 set(gcf, 'PaperPosition', [0 0 20 5]);
@@ -185,16 +183,83 @@ saveas(gcf,'schaub16.pdf')
 clc;clear;
 
 %%%%%%%%%%%%%%%%%%
-% Random network with 4 symmetry clusters from Fig. 3 of
+% Network with 2 equitable clusters from Fig.1 of
 % Y. Zhang and A. E. Motter, 
-% Fast, unified, and direct approach to network synchronization
+% Fast and symmetry-independent stability analysis
+% of cluster synchronization patterns
+%%%%%%%%%%%%%%%%%%
+
+n = 15; %% size of the matrices
+%% define adjacency matrix
+adj = [0,1,1,0,0,0,0,0,0,0,0,1,0,0,0;...
+		1,0,1,0,0,0,0,1,0,0,0,0,0,0,0;...
+		1,1,0,1,0,0,0,0,0,0,0,0,0,0,0;...
+		0,0,1,0,1,1,1,0,0,0,0,0,0,0,0;...
+		0,0,0,1,0,0,1,0,0,0,0,0,0,0,0;...
+		0,0,0,1,0,0,1,0,0,0,0,0,0,0,0;...
+		0,0,0,1,1,1,0,0,0,0,0,0,0,0,0;...
+		0,1,0,0,0,0,0,0,1,1,1,0,0,0,0;...
+		0,0,0,0,0,0,0,1,0,0,0,0,0,0,0;...
+		0,0,0,0,0,0,0,1,0,0,0,0,0,0,0;...
+		0,0,0,0,0,0,0,1,0,0,0,0,0,0,0;...
+		1,0,0,0,0,0,0,0,0,0,0,0,1,1,1;...
+		0,0,0,0,0,0,0,0,0,0,0,1,0,1,1;...
+		0,0,0,0,0,0,0,0,0,0,0,1,1,0,1;...
+		0,0,0,0,0,0,0,0,0,0,0,1,1,1,0];
+
+%% define Laplacian matrix
+rowsum = sum(adj,2);
+laplacian = diag(rowsum) - adj;
+%% add noise/error to the matrix data
+D{1} = laplacian + 1e-5*rand(n);
+
+%% D{2} and D{3} are diagonal matrices that encode cluster info
+%% e.g., nodes 4, 8 and 12 are in the same equitable cluster
+D{2} = zeros(n);
+D{2}(4,4) = 1;
+D{2}(8,8) = 1;
+D{2}(12,12) = 1;
+
+D{3} = eye(n) - D{2};
+
+
+%% calculate orthogonal matrix for simultaneous block-diagonalization
+P = sbd_fast(D,'real',1e-2);
+for ii = 1:3
+	D{ii} = P' * D{ii} * P;
+end
+
+%% plot the matrices in common block-diagonal form
+figure
+colormap(flipud(colormap));
+set(gcf, 'PaperPosition', [0 0 20 5]);
+set(gcf, 'PaperSize', [20 5]);
+for ii = 1:3
+	subplot(1,3,ii)
+	imagesc(D{ii});
+	caxis([-max(D{ii}(:)) max(D{ii}(:))])
+	colormap(lbmap(201,'RedWhiteBlue'))
+	set(gca,'XTick',[])
+	set(gca,'YTick',[])
+end
+%set(gca,'Visible','off');
+saveas(gcf,'Fig1_Zhang.pdf')
+
+
+clc;clear;
+
+%%%%%%%%%%%%%%%%%%
+% Random network with 4 symmetry clusters from Fig.3 of
+% Y. Zhang and A. E. Motter, 
+% Fast and symmetry-independent stability analysis
+% of cluster synchronization patterns
 %%%%%%%%%%%%%%%%%%
 
 n = 30; %% size of the matrices
 %% define adjacency matrix
 adj = dlmread('random_net.txt');
 %% add noise/error to the matrix data
-D{1} = adj + 1e-7*rand(n);
+D{1} = adj + 1e-5*rand(n);
 
 %% D{2} to D{5} are diagonal matrices that encode cluster info
 %% e.g., node 20 and node 24 are in the same symmetry cluster
@@ -217,13 +282,12 @@ D{4}(11,11) = 1;
 D{5} = eye(n) - D{2} - D{3} - D{4};
 
 %% calculate orthogonal matrix for simultaneous block-diagonalization
-P = sbd_fast(D,'real',1e-4);
+P = sbd_fast(D,'real',1e-2);
 for ii = 1:5
 	D{ii} = P' * D{ii} * P;
 end
 
-set(0,'DefaultAxesFontSize',25)
-
+%% plot the matrices in common block-diagonal form
 figure
 colormap(flipud(colormap));
 set(gcf, 'PaperPosition', [0 0 8 5]);
@@ -243,45 +307,68 @@ saveas(gcf,'Fig3_Zhang.pdf')
 clc;clear;
 
 %%%%%%%%%%%%%%%%%%
-% Ring network with 2 types of oscillators from Fig. 4 of
+% Chimera states in a multilayer network from Fig.4 of
 % Y. Zhang and A. E. Motter, 
-% Fast, unified, and direct approach to network synchronization
+% Fast and symmetry-independent stability analysis
+% of cluster synchronization patterns
 %%%%%%%%%%%%%%%%%%
 
-n = 20; %% size of the matrices
-%% define adjacency matrix
-adj = zeros(1,n);
-adj(1,2) = 1; adj(1,end) = 1;
-ring = adj;
-for ii = 1:n-1
-  adj = [adj;circshift(ring,[0,ii])];
+n = 6; %% size of the matrices representing each layer
+
+%% first-layer subnetwork
+adj1 = [0,1,0,0,1,1;...
+		1,0,0,0,1,0;...
+		0,0,0,1,1,0;...
+		0,0,1,0,1,1;...
+		1,1,1,1,0,0;...
+		1,0,0,1,0,0];
+
+%% second-layer subnetwork
+adj2 = [0,1,0,1,0,0;...
+		1,0,1,0,1,0;...
+		0,1,0,1,1,0;...
+		1,0,1,0,1,0;...
+		0,1,1,1,0,1;...
+		0,0,0,0,1,0];
+
+%% adjacency matrices representing the interlayer and intralayer connections
+aij1 = [adj1,zeros(n); zeros(n),adj2];
+aij2 = [zeros(n),ones(n); ones(n),zeros(n)];
+
+%% laplacian matrices representing the interlayer and intralayer connections
+rowsum = sum(aij1,2);
+laplacian1 = diag(rowsum) - aij1;
+rowsum = sum(aij2,2);
+laplacian2 = diag(rowsum) - aij2;
+%% add noise/error to the matrix data
+D{1} = laplacian1 + 1e-5*rand(2*n);
+D{2} = laplacian2 + 1e-5*rand(2*n);
+
+%% D{3} and D{4} are diagonal matrices that encode cluster info
+%% e.g., nodes 1 to n are in the same cluster
+D{3} = zeros(2*n);
+for i = 1:n
+	D{3}(i,i) = 1;
 end
-D{1} = adj;
 
-
-%% D{2} and D{3} are diagonal matrices that encode the oscillator type
-%% (arranged alternatingly in this case)
-D{2} = zeros(n);
-for ii = 1:n/2
-  D{2}(2*ii,2*ii) = 1;
+D{4} = zeros(2*n);
+for i = 1:n
+	D{4}(n+i,n+i) = 1;
 end
-
-D{3} = eye(n) - D{2};
 
 %% calculate orthogonal matrix for simultaneous block-diagonalization
-P = sbd_fast(D,'real',1e-8);
-for ii = 1:3
+P = sbd_fast(D,'real',1e-2);
+for ii = 1:4
 	D{ii} = P' * D{ii} * P;
 end
 
-set(0,'DefaultAxesFontSize',25)
-
+%% plot the matrices in common block-diagonal form
 figure
 colormap(flipud(colormap));
-set(gcf, 'PaperPosition', [0 0 20 5]);
-set(gcf, 'PaperSize', [20 5]);
-for ii = 1:3
-	subplot(1,3,ii)
+set(gcf, 'PaperPosition', [0 0 12 12]);
+set(gcf, 'PaperSize', [12 12]);
+for ii = 1:4
+	subplot(2,2,ii)
 	imagesc(D{ii});
 	caxis([-max(D{ii}(:)) max(D{ii}(:))])
 	colormap(lbmap(201,'RedWhiteBlue'))
@@ -289,65 +376,50 @@ for ii = 1:3
 	set(gca,'YTick',[])
 end
 %set(gca,'Visible','off');
-saveas(gcf,'Fig4_Zhang.pdf')
+saveas(gcf,'Fig6b_Zhang.pdf')
 
+%% We now study the 7-cluster pattern in the same multilayer network
+D{1} = laplacian1 + 1e-5*rand(2*n);
+D{2} = laplacian2 + 1e-5*rand(2*n);
 
-clc;clear;
-
-%%%%%%%%%%%%%%%%%%
-% Network with 2 types of diffusive interactions from Fig. 6 of
-% Y. Zhang and A. E. Motter, 
-% Fast, unified, and direct approach to network synchronization
-%%%%%%%%%%%%%%%%%%
-
-n = 16; %% size of the matrices
-
-% define adjacency matrix for first type of interaction
-adj = zeros(1,n);
-adj(1,2) = 1; adj(1,end) = 1; adj(1,9) = 1;
-ring = adj;
-for ii = 1:n-1
-  adj = [adj;circshift(ring,[0,ii])];
+%% D{3} to D{9} are diagonal matrices that encode cluster info
+%% e.g., nodes 1 to n are in the same cluster
+D{3} = zeros(2*n);
+for i = 1:n
+	D{3}(i,i) = 1;
 end
-% define Laplacian matrix for first type of interaction
-rowsum = sum(adj,2);
-laplacian = diag(rowsum) - adj;
-D{1} = laplacian;
 
-% define adjacency matrix for second type of interaction
-adj = zeros(1,n);
-adj(1,3) = 1; adj(1,end-1) = 1;
-ring = adj;
-for ii = 1:n-1
-  adj = [adj;circshift(ring,[0,ii])];
-end
-adj(1,11) = 1; 	adj(11,1) = 1;
-adj(3,9) = 1; 	adj(9,3) = 1;
-adj(5,15) = 1; 	adj(15,5) = 1;
-adj(7,13) = 1; 	adj(13,7) = 1;
-adj(2,8) = 1; 	adj(8,2) = 1;
-adj(4,14) = 1; 	adj(14,4) = 1;
-adj(6,12) = 1; 	adj(12,6) = 1;
-adj(10,16) = 1; adj(16,10) = 1;
-% define Laplacian matrix for second type of interaction
-rowsum = sum(adj,2);
-laplacian = diag(rowsum) - adj;
-D{2} = laplacian;
+D{4} = zeros(2*n);
+D{4}(n+1,n+1) = 1;
+
+D{5} = zeros(2*n);
+D{5}(n+2,n+2) = 1;
+
+D{6} = zeros(2*n);
+D{6}(n+3,n+3) = 1;
+
+D{7} = zeros(2*n);
+D{7}(n+4,n+4) = 1;
+
+D{8} = zeros(2*n);
+D{8}(n+5,n+5) = 1;
+
+D{9} = zeros(2*n);
+D{9}(n+6,n+6) = 1;
 
 %% calculate orthogonal matrix for simultaneous block-diagonalization
-P = sbd_fast(D,'real',1e-8);
-for ii = 1:2
+P = sbd_fast(D,'real',1e-2);
+for ii = 1:9
 	D{ii} = P' * D{ii} * P;
 end
 
-set(0,'DefaultAxesFontSize',25)
-
+%% plot the matrices in common block-diagonal form
 figure
 colormap(flipud(colormap));
-set(gcf, 'PaperPosition', [0 0 12 5]);
-set(gcf, 'PaperSize', [12 5]);
-for ii = 1:2
-	subplot(1,2,ii)
+set(gcf, 'PaperPosition', [0 0 12 12]);
+set(gcf, 'PaperSize', [12 12]);
+for ii = 1:9
+	subplot(3,3,ii)
 	imagesc(D{ii});
 	caxis([-max(D{ii}(:)) max(D{ii}(:))])
 	colormap(lbmap(201,'RedWhiteBlue'))
@@ -355,4 +427,4 @@ for ii = 1:2
 	set(gca,'YTick',[])
 end
 %set(gca,'Visible','off');
-saveas(gcf,'Fig6_Zhang.pdf')
+saveas(gcf,'Fig6c_Zhang.pdf')
